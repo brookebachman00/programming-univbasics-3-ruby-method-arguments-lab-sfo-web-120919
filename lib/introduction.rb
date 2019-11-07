2fcfebe1d1759b02d098f2317759d525769f562c
Brooke Bachman <bbachman1994@gmail.com>
2019-11-07 22:58:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,9 +2,14 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
- puts "#{name}"
 
-def language(name,language )
+def one(name)
+  puts "#{name}"
+end
+
+language = ruby
+
+def method(name,language )
   puts "hello #{name} + #{language}"
 end
 
